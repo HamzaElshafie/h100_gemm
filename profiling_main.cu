b6a6cb35738dd3fe5fdc994ad044a774b646a355
HamzaElshafie <hamzaelshafie@outlook.com>
2025-08-18 21:55:04 +0300

    went back to warp tiling without shuffle as it made perf. worse.

--- profiling_main.cu
+++ profiling_main.cu
@@ -6,61 +6,219 @@
 
 #include "utils.h"
 
+#define WARPSIZE 32
 
-template <const uint TILE_SIZE>
-__global__ void sgemm_tiled_shared(const float* __restrict__ A, const float* __restrict__ B, float* __restrict__ C,
-    int M, int N, int K, float alpha, float beta) {
-        // Allocate shared memory
-        __shared__ float sharedA[TILE_SIZE * TILE_SIZE];
-        __shared__ float sharedB[TILE_SIZE * TILE_SIZE];
+template <const uint TILE_SIZE_M, const uint TILE_SIZE_N, const uint TILE_SIZE_K,
+          const uint WARP_TILE_M, const uint WARP_TILE_K, const uint WARP_STEPS_K,
+          const uint ROWS_PER_THREAD, const uint COLS_PER_THREAD, const uint NUM_THREADS>
+__global__ void __launch_bounds__(NUM_THREADS)
+sgemm_warptiling(const float* __restrict__ A, const float* __restrict__ B, float* __restrict__ C,
+                 int M, int N, int K, float alpha, float beta) {
 
-        // Identify the tile of C this thread block is responsible for (We assume tiles are same size as block)
-        const uint block_row = blockIdx.y;
-        const uint block_column = blockIdx.x;
+    // Allocate shared memory. Use padded leading strides that keep float4 alignment
+    constexpr uint STRIDE_A = (TILE_SIZE_M % 32u == 0u) ? (TILE_SIZE_M + 4u) : TILE_SIZE_M;
+    constexpr uint STRIDE_B = (TILE_SIZE_K % 32u == 0u) ? (TILE_SIZE_K + 4u) : TILE_SIZE_K;
+    static_assert((STRIDE_A % 4u) == 0u, "STRIDE_A must keep float4 alignment");
+    static_assert((STRIDE_B % 4u) == 0u, "STRIDE_B must keep float4 alignment");
 
-        // Calculate position of thread within tile (Remapping from 1-D to 2-D)
-        const uint ty = threadIdx.x / TILE_SIZE; // (0, TILE_SIZE-1)
-        const uint tx = threadIdx.x % TILE_SIZE; // (0, TILE_SIZE-1)
+    __shared__ float sharedA[TILE_SIZE_N * STRIDE_A];
+    __shared__ float sharedB[TILE_SIZE_N * STRIDE_B];
 
-        // Move pointers from A[0], B[0] and C[0] to the starting positions of the tile
-        A += block_row * TILE_SIZE * N; // Move pointer (block_row * TILE_SIZE) rows down
-        B += block_column * TILE_SIZE; // Move pointer (block_column * TILE_SIZE) columns to the right 
-        C += (block_row * TILE_SIZE * K) + (block_column * TILE_SIZE); // Move pointer (block_row * TILE_SIZE * K) rows down then (block_column * TILE_SIZE) columns to the right
+    // Identify the tile of C this thread block is responsible for
+    const uint block_row = blockIdx.y;
+    const uint block_column = blockIdx.x;
 
-        // Calculate how many tiles we have
-        const uint num_tiles = (K + TILE_SIZE - 1) / TILE_SIZE;
-        float cumulative_sum = 0.0f;
+    constexpr uint WARP_STEPS_M = (WARP_TILE_M * WARP_TILE_K) / (WARPSIZE * ROWS_PER_THREAD * COLS_PER_THREAD * WARP_STEPS_K);
+    // Warp subtile is WARP_SUB_M x WARP_SUB_K
+    constexpr uint WARP_SUB_M = WARP_TILE_M / WARP_STEPS_M; // 64 / 1 = 64
+    constexpr uint WARP_SUB_K = WARP_TILE_K / WARP_STEPS_K; // 64 / 4 = 16
 
-        // Iterate over tiles (Phase 1: Loading data)
-        for (int t = 0; t < num_tiles; t++) {
-            sharedA[ty * TILE_SIZE + tx] = A[ty * N + tx];
-            sharedB[ty * TILE_SIZE + tx] = B[ty * K + tx];
+    // Identify the warp tile position
+    const uint warp_idx = threadIdx.x / WARPSIZE;
+    const uint warps_per_row = TILE_SIZE_K / WARP_TILE_K;
+    const uint warp_row = warp_idx / warps_per_row;
+    const uint warp_col = warp_idx % warps_per_row;
 
-            // Barrier synchronisation until all threads load smem tiles
-            __syncthreads();
+    // Identify the thread position within the warp tile
+    uint lane = threadIdx.x % WARPSIZE;
+    const uint threads_per_subtile_row = WARP_SUB_K / COLS_PER_THREAD; // 16/4 = 4 threads per row
+    const uint thread_row_in_sub = lane / threads_per_subtile_row;
+    const uint thread_col_in_sub = lane % threads_per_subtile_row;
 
-            // Phase 2: Compute partial results iteratively
-            for (int i = 0; i < TILE_SIZE; i++) {
-                cumulative_sum += sharedA[ty * TILE_SIZE + i] * sharedB[i * TILE_SIZE + tx];
-            }
+    const uint ty = thread_row_in_sub;
+    const uint tx = thread_col_in_sub;
 
-            // Barrier synchronisation until all threads finish writing to smem
-            __syncthreads();
+    // Move pointers from A[0], B[0] and C[0] to the starting positions of the tile
+    A += block_row * TILE_SIZE_M * N;
+    B += block_column * TILE_SIZE_K;
+    C += (block_row * TILE_SIZE_M * K) + (block_column * TILE_SIZE_K);
 
-            // Move all pointers to the starting positions of the next tile
-            A += TILE_SIZE; // Move right
-            B += TILE_SIZE * K; // Move down
+    const uint VEC_CHUNKS_N = TILE_SIZE_N / 4; // 16 / 4 = 4
+    const uint VEC_CHUNKS_K = TILE_SIZE_K / 4; // 128 / 4 = 32
+
+    // Map each thread to one 4-float chunk that it will load. We will have to offset as we have fewer threads than elements to cover (offset by 32)
+    const uint smem_ty_A = threadIdx.x / VEC_CHUNKS_N; // --> 0, ..., 31
+    const uint smem_tx_A = threadIdx.x % VEC_CHUNKS_N; // --> 0, 1, 2, 3
+    // If we give each thread a vector load of 4 elements along TILE_SIZE_N, how many different rows of sharedA can we cover in one pass through all the threads?
+    const uint strideA = (NUM_THREADS * 4) / TILE_SIZE_N; // 32 rows per pass
+
+    const uint smem_ty_B = threadIdx.x / VEC_CHUNKS_K; // --> 0, 1, 2, 3
+    const uint smem_tx_B = threadIdx.x % VEC_CHUNKS_K; // --> 0, ...., 31
+    // If we give each thread a vector load of 4 elements along TILE_SIZE_K, how many different rows of sharedB can we cover in one pass through all the threads?
+    const uint strideB = (NUM_THREADS * 4) / TILE_SIZE_K; // 4 rows per pass
+
+    float thread_results[WARP_STEPS_M * ROWS_PER_THREAD * WARP_STEPS_K * COLS_PER_THREAD] = {0.0f};
+    float reg_m[WARP_STEPS_M * ROWS_PER_THREAD];
+    float reg_k[WARP_STEPS_K * COLS_PER_THREAD];
+
+    const uint num_tiles = CEIL_DIV(N, TILE_SIZE_N);
+
+    // Outer loop iterate over tiles
+    for (int t = 0; t < num_tiles; t++) {
+        // Populate smem using vectorised loads (We use offsets and is transposed)
+        for (int load_offset = 0; load_offset < TILE_SIZE_M; load_offset += strideA) {
+            const float4 v = reinterpret_cast<const float4*>(
+                &A[(smem_ty_A + load_offset) * N + smem_tx_A * 4])[0];
+
+            // Transpose A (instead of 128x16 previously for ex, now it will be 16x128)
+            sharedA[(smem_tx_A * 4 + 0) * STRIDE_A + (smem_ty_A + load_offset)] = v.x;
+            sharedA[(smem_tx_A * 4 + 1) * STRIDE_A + (smem_ty_A + load_offset)] = v.y;
+            sharedA[(smem_tx_A * 4 + 2) * STRIDE_A + (smem_ty_A + load_offset)] = v.z;
+            sharedA[(smem_tx_A * 4 + 3) * STRIDE_A + (smem_ty_A + load_offset)] = v.w;
         }
-        // Write results back to C
-        C[ty * K + tx] = (alpha * cumulative_sum) + (beta * C[ty * K + tx]);
+
+        // Load from as B as well but without transposing
+        for (int load_offset = 0; load_offset < TILE_SIZE_N; load_offset += strideB) {
+            reinterpret_cast<float4*>(
+                &sharedB[(smem_ty_B + load_offset) * STRIDE_B + smem_tx_B * 4])[0] =
+            reinterpret_cast<const float4*>(
+                &B[(smem_ty_B + load_offset) * K + smem_tx_B * 4])[0];
+        }
+
+        __syncthreads();
+
+        // Iterate over the shared dimension of the SMEM tiles
+        for (int i = 0; i < TILE_SIZE_N; i++) {
+            // Load slice at current i iteration in sharedA's register
+            for (int wSubRow = 0; wSubRow < WARP_STEPS_M; wSubRow++) {
+                uint base_row = (warp_row * WARP_TILE_M) + (wSubRow * WARP_SUB_M) + (ty * ROWS_PER_THREAD);
+
+                // Each row "leader" load from sharedA, others will receive via shuffle
+                const bool is_row_leader = (tx == 0);
+                if (is_row_leader) {
+                    #pragma unroll
+                    for (int row = 0; row < ROWS_PER_THREAD; row += 4) {
+                        const float4 va = reinterpret_cast<const float4*>(
+                            &sharedA[i * STRIDE_A + base_row + row])[0];
+
+                        reg_m[wSubRow * ROWS_PER_THREAD + row + 0] = va.x;
+                        reg_m[wSubRow * ROWS_PER_THREAD + row + 1] = va.y;
+                        reg_m[wSubRow * ROWS_PER_THREAD + row + 2] = va.z;
+                        reg_m[wSubRow * ROWS_PER_THREAD + row + 3] = va.w;
+                    }
+                }
+
+                for (int wSubCol = 0; wSubCol < WARP_STEPS_K; wSubCol++) {
+                    uint col_base = (warp_col * WARP_TILE_K) + (wSubCol * WARP_SUB_K) + (tx * COLS_PER_THREAD);
+
+                    // Only column leaders fill the register
+                    const bool is_col_leader = (ty == 0);
+                    if (is_col_leader) {
+                        #pragma unroll
+                        for (int col = 0; col < COLS_PER_THREAD; col += 4) {
+                            const float4 vb = reinterpret_cast<const float4*>(
+                                &sharedB[i * STRIDE_B + col_base + col])[0];
+
+                            reg_k[wSubCol * COLS_PER_THREAD + col + 0] = vb.x;
+                            reg_k[wSubCol * COLS_PER_THREAD + col + 1] = vb.y;
+                            reg_k[wSubCol * COLS_PER_THREAD + col + 2] = vb.z;
+                            reg_k[wSubCol * COLS_PER_THREAD + col + 3] = vb.w;
+                        }
+                    }
+                    // broadcast from leader, then compute outer product
+                    const unsigned mask = __activemask();
+                    const uint row_leader_lane = ty * threads_per_subtile_row + 0;
+                    const uint col_leader_lane = 0 * threads_per_subtile_row + tx;
+                    #pragma unroll
+                    for (int im = 0; im < ROWS_PER_THREAD; im++) {
+                        float a_src = is_row_leader? reg_m[wSubRow * ROWS_PER_THREAD + im] : 0.0f;
+                        float a_val = __shfl_sync(mask, a_src, row_leader_lane);
+                        #pragma unroll
+                        for (int ik = 0; ik < COLS_PER_THREAD; ik++) {
+                            float b_src = is_col_leader ? reg_k[wSubCol * COLS_PER_THREAD + ik] : 0.f;
+                            float b_val = __shfl_sync(mask, b_src, col_leader_lane);
+                            const uint out_idx = (wSubRow * ROWS_PER_THREAD + im) * (WARP_STEPS_K * COLS_PER_THREAD) + (wSubCol * COLS_PER_THREAD + ik);
+                            thread_results[out_idx] += a_val * b_val;
+                        }
+                    }
+                }            
+            }
+        }
+        __syncthreads();
+
+        A += TILE_SIZE_N;  // Move right
+        B += TILE_SIZE_N * K; // Move down
     }
 
-void launch_sgemm_tiled_shared(const float* A, const float* B, float* C, int M, int N, int K, float alpha, float beta) {
-    const int TILE_SIZE = 32;
-    dim3 gridDim(CEIL_DIV(K, TILE_SIZE), CEIL_DIV(M, TILE_SIZE));
-    dim3 blockDim(TILE_SIZE * TILE_SIZE); // 1024 threads per block
-    
-    sgemm_tiled_shared<TILE_SIZE><<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
+    // Write results of the thread back to C
+    float* C_tile_base = C; // Keep base pointer
+    for (int wSubRow = 0; wSubRow < WARP_STEPS_M; wSubRow++) {
+        for (int wSubCol = 0; wSubCol < WARP_STEPS_K; wSubCol++) {
+            float* C_ptr = C_tile_base
+                         + (warp_row * WARP_TILE_M + wSubRow * WARP_SUB_M) * K
+                         + (warp_col * WARP_TILE_K + wSubCol * WARP_SUB_K);
+
+            #pragma unroll
+            for (int im = 0; im < ROWS_PER_THREAD; im++) {
+                int c_row = ty * ROWS_PER_THREAD + im;
+                #pragma unroll
+                for (int ik = 0; ik < COLS_PER_THREAD; ik += 4) {
+                    int c_col = tx * COLS_PER_THREAD + ik;
+
+                    uint idx_out = (wSubRow * ROWS_PER_THREAD + im) * (WARP_STEPS_K * COLS_PER_THREAD)
+                                   + (wSubCol * COLS_PER_THREAD + ik);
+
+                    float4 temp_out;
+                    temp_out.x = thread_results[idx_out + 0];
+                    temp_out.y = thread_results[idx_out + 1];
+                    temp_out.z = thread_results[idx_out + 2];
+                    temp_out.w = thread_results[idx_out + 3];
+
+                    float4 vc = reinterpret_cast<float4*>(&C_ptr[c_row * K + c_col])[0];
+
+                    vc.x = alpha * temp_out.x + beta * vc.x;
+                    vc.y = alpha * temp_out.y + beta * vc.y;
+                    vc.z = alpha * temp_out.z + beta * vc.z;
+                    vc.w = alpha * temp_out.w + beta * vc.w;
+
+                    reinterpret_cast<float4*>(&C_ptr[c_row * K + c_col])[0] = vc;
+                }
+            }
+        }
+    }
+}
+
+
+void launch_sgemm_warptiling(const float* A, const float* B, float* C, int M, int N, int K, float alpha, float beta) {
+    constexpr int TILE_SIZE_M = 128;
+    constexpr int TILE_SIZE_N = 16;
+    constexpr int TILE_SIZE_K = 128;
+    constexpr int WARP_TILE_M = 64;
+    constexpr int WARP_TILE_K = 64;
+    constexpr int ROWS_PER_THREAD = 4;
+    constexpr int COLS_PER_THREAD = 4;
+    constexpr int WARP_STEPS_K = 2;
+    constexpr int NUM_THREADS = 128;
+
+    dim3 blockDim(NUM_THREADS);
+    dim3 gridDim(CEIL_DIV(K, TILE_SIZE_K), CEIL_DIV(M, TILE_SIZE_M));
+
+    sgemm_warptiling<
+        TILE_SIZE_M, TILE_SIZE_N, TILE_SIZE_K,
+        WARP_TILE_M, WARP_TILE_K, WARP_STEPS_K,
+        ROWS_PER_THREAD, COLS_PER_THREAD, NUM_THREADS>
+        <<<gridDim, blockDim>>>(A, B, C, M, N, K, alpha, beta);
+
     CUDA_CHECK(cudaGetLastError());
     CUDA_CHECK(cudaDeviceSynchronize());
 }
@@ -69,7 +227,7 @@ void launch_sgemm_tiled_shared(const float* A, const float* B, float* C, int M, 
  * @brief Main entry point for the profiling program.
  */
 int main(int argc, char** argv) {
-    ResourceManager resources;
+    ResourceManager<float> resources;
 
     // Fixed size for profiling
     const int size = 8192;
@@ -78,7 +236,7 @@ int main(int argc, char** argv) {
     
     const size_t mem_size = size * size * sizeof(float);
 
-    std::cout << "Profiling sgemm_tiled_shared kernel with matrix size " << size << "x" << size << std::endl;
+    std::cout << "Profiling sgemm_warptiling kernel with matrix size " << size << "x" << size << std::endl;
 
     // Allocate host memory
     float* A_host = (float*)malloc(mem_size);
@@ -97,7 +255,7 @@ int main(int argc, char** argv) {
 
     // Initialise matrices
     float* matrices[] = {A_host, B_host, C_host};
-    initialiseArrays(matrices, 3, size * size, -100.0f, 100.0f, 0);
+    initialiseArrays<float>(matrices, 3, size * size, -100.0f, 100.0f, 0);
 
     // Allocate device memory
     float* A_device;
@@ -122,16 +280,16 @@ int main(int argc, char** argv) {
     // Warm-up launches
     std::cout << "Running warm-up launches..." << std::endl;
     for (int i = 0; i < 2; ++i) {
-        launch_sgemm_tiled_shared(A_device, B_device, C_device, size, size, size, alpha, beta);
+        launch_sgemm_warptiling(A_device, B_device, C_device, size, size, size, alpha, beta);
         CUDA_CHECK(cudaDeviceSynchronize());
     }
 
     // Main kernel launch for profiling
     std::cout << "Running main kernel for profiling..." << std::endl;
-    launch_sgemm_tiled_shared(A_device, B_device, C_device, size, size, size, alpha, beta);
+    launch_sgemm_warptiling(A_device, B_device, C_device, size, size, size, alpha, beta);
     CUDA_CHECK(cudaDeviceSynchronize());
 
     std::cout << "Kernel launch completed successfully" << std::endl;
 
-    return 0;  // ResourceManager destructor will all handle cleanups
+    return 0;  // ResourceManager destructor will handle cleanups
 }
