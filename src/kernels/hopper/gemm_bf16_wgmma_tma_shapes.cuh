f99b6ffb3d04b01b9142fbe90177d24be3da1753
HamzaElshafie <hamzaelshafie@outlook.com>
2026-01-12 10:22:41 +0200

    Kernel 8: compute phase done.

--- src/kernels/hopper/gemm_bf16_wgmma_tma_shapes.cuh
+++ src/kernels/hopper/gemm_bf16_wgmma_tma_shapes.cuh
@@ -14,6 +14,12 @@ using bf16 = __nv_bfloat16;
 using barrier = cuda::barrier<cuda::thread_scope_block>;
 namespace cde = cuda::device::experimental;
 
+template <int TILE_SIZE_M, int TILE_SIZE_K, int TILE_SIZE_N>
+struct SMem {
+    alignas(128) bf16 A[TILE_SIZE_M * TILE_SIZE_K];
+    alignas(128) bf16 B[TILE_SIZE_K * TILE_SIZE_N];
+};
+
 template <const uint TILE_SIZE_M, const uint TILE_SIZE_K, const uint TILE_SIZE_N,
           const uint WGMMA_M, const uint WGMMA_K, const uint WGMMA_N, const uint NUM_THREADS>
 __global__ void __launch_bounds__(NUM_THREADS)
@@ -21,14 +27,26 @@ gemm_bf16_wgmma_tma_shapes(const CUtensorMap* tensorMapA, const CUtensorMap* ten
     int M, int K, int N, float alpha, float beta) {
         // assert WGMMA_N = TILE_SIZE_N
         static_assert(WGMMA_N == TILE_SIZE_N, "WGMMA_N must be == TILE_SIZE_N");
+        static_assert(NUM_THREADS % 128 == 0, "NUM_THREADS must be divisible by warp group size (128)");
+        static_assert(TILE_SIZE_K % WGMMA_K == 0, "TILE_SIZE_K must be divisible by WGMMA_K");
 
         // Allocate SMEM 
-        __shared__ alignas(128) bf16 sharedA[TILE_SIZE_M * TILE_SIZE_K];
-        __shared__ alignas(128) bf16 sharedB[TILE_SIZE_K * TILE_SIZE_N];
+        extern __shared__ SMem<TILE_SIZE_M, TILE_SIZE_K, TILE_SIZE_N> s;
+        bf16* sharedA = s.A;
+        bf16* sharedB = s.B;
 
         // Init each thread's register
         float d[TILE_SIZE_M / WGMMA_M][WGMMA_N / WGMMA_K][8]; // Where WGMMA_K will always be 16
         memset(d, 0, sizeof(d));
+
+        // How many warp groups are in this block (one warp group = 128 threads)
+        constexpr int num_warp_groups = NUM_THREADS / 128;
+
+        // How many M rows of the output tile each warp group is responsible for
+        // @example: TILE_SIZE_M = 128, NUM_THREADS = 256 -> num_warp_groups = 2 -> 64 rows per warp group
+        constexpr int rows_per_warp_group = TILE_SIZE_M / num_warp_groups;
+
+        int warp_group_idx = threadIdx.x / 128;
 
         const int num_blocks_k = CEIL_DIV(K, TILE_SIZE_K);
         int num_block_n = blockIdx.x % CEIL_DIV(N, TILE_SIZE_N);
@@ -62,9 +80,9 @@ gemm_bf16_wgmma_tma_shapes(const CUtensorMap* tensorMapA, const CUtensorMap* ten
                 // Initiate bulk tensor copy.
                 cde::cp_async_bulk_tensor_2d_global_to_shared(&sharedA[0], tensorMapA, block_k_iter * TILE_SIZE_K, num_block_m * TILE_SIZE_M, barA);
                 // Arrive on the barrier and tell how many bytes are expected to come in.
-                tokenA = cuda::device::barrier_arrive_tx(barA, 1, sizeof(sharedA));
+                tokenA = cuda::device::barrier_arrive_tx(barA, 1, sizeof(s.A));
                 cde::cp_async_bulk_tensor_2d_global_to_shared(&sharedB[0], tensorMapB, block_k_iter * TILE_SIZE_K, num_block_n * TILE_SIZE_N, barB);
-                tokenB = cuda::device::barrier_arrive_tx(barB, 1, sizeof(sharedB));
+                tokenB = cuda::device::barrier_arrive_tx(barB, 1, sizeof(s.B));
             } else {
                 // Other threads just arrive.
                 tokenA = barA.arrive();
@@ -75,10 +93,32 @@ gemm_bf16_wgmma_tma_shapes(const CUtensorMap* tensorMapA, const CUtensorMap* ten
             barB.wait(std::move(tokenB));
             __syncthreads();
 
-            // 2. Compute phase
-            // TODO
+            // 2. Compute phase using WGMMA tensor cores instructions
+            warpgroup_arrive();
+            // Outer loop over TILE_SIZE_M in WGMMA_M steps
+            // If we have two warp groups, we let each work on a different partition of TILE_SIZE_M
+            // @example: 
+            #pragma unroll
+            for (int m_iter = 0; m_iter < rows_per_warp_group / WGMMA_M; m_iter++) {
+                bf16* sharedA_wgmma_tile_base = sharedA + ((warp_group_idx * rows_per_warp_group) + (m_iter * WGMMA_M)) * TILE_SIZE_K;
+                // Inner loop iterating over TILE_SIZE_K in WGMMA_K steps
+                #pragma unroll
+                for (int k_iter = 0; k_iter < TILE_SIZE_K / WGMMA_K; k_iter++) {
+                    wgmma<WGMMA_N, 1, 1, 1, 0, 0>(d[m_iter], &sharedA_wgmma_tile_base[k_iter * WGMMA_K], &sharedB[k_iter * WGMMA_K]);
+                }
+            }
+            warpgroup_commit_batch(); // asm volatile("wgmma.commit_group.sync.aligned;\n" ::: "memory");
+            warpgroup_wait<0>();      // asm volatile("wgmma.wait_group.sync.aligned %0;\n" ::"n"(N) : "memory");
         }
 
         // 3. Epilogue store phase
-        // TODO
+        int tid = threadIdx.x % 128; // “folds” threads so the indexing logic assumes a single 128 thread warp group.
+        int lane = tid % 32;
+        int warp = tid / 32;
+        uint32_t row = warp * 16 + lane / 4;
+        // @note C is column-major
+        bf16* block_C = C + (num_block_n * TILE_SIZE_N * M) + (num_block_m * TILE_SIZE_M);
+        for (int m_iter = 0; m_iter < rows_per_warp_group / WGMMA_M; m_iter++) {
+            int row_tile_base_C = C + (warp_group_idx * rows_per_warp_group) + (m_iter * WGMMA_M) * M;
+        }
     }
